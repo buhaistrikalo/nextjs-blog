67739d9f165ab11b7d07a2964d484909f937700b
Kirill <kirilltcoi@yandex.ru>
2022-07-08 11:33:52 +0700

    Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from 'styles/Navbar.module.css'
+import styles from 'styles/Navbar.module.scss';
+import Center from './Center';
+import Iconify from './Iconify';
 
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
-            <Link className={styles.navbar__link} href="/">
-                <a className={styles.navbar__link}>Home</a>
-            </Link>
-            <Link className={styles.navbar__link} href="/users">
-                <a className={styles.navbar__link}>Users</a>
-            </Link>
+            <Center>
+                <Link href="/">
+                    <a className={styles.navbar__link}>Home</a>
+                </Link>
+                <Link href="/404">
+                    <a className={styles.navbar__link}>404</a>
+                </Link>
+                <Link href="https://github.com/buhaistrikalo" style={{ color: '#7247ca' }}>
+                    <div className={styles.navbar__link}>
+                        <Iconify
+                            icon="akar-icons:github-fill"
+                            style={{ fontSize: '20px', color: '#7247ca', marginRight: '10px' }}
+                        />
+                        <a
+                            style={{
+                                color: '#7247ca',
+                            }}>
+                            Github
+                        </a>
+                    </div>
+                </Link>
+            </Center>
         </nav>
     );
 };
